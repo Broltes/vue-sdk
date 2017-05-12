05cba0bc8cbf18fde0040a836e660ef2432d2a1d
Broltes <Broltes@Broltes-MacBook-Pro.local>
2017-05-12 16:24:23 +0800

    fix dialog on reuse, the title or content not reset if omitted

--- src/plugins/dialog.js
+++ src/plugins/dialog.js
@@ -16,17 +16,21 @@ function init() {
  * @param {Object} options
  * @param {String} options.title
  * @param {String} options.content
- * @param {Array} options.buttons list of button text
+ * @param {Array} options.buttons List of button text
  * @param {Function} options.action
  */
 function dialog(options) {
   if (!$vm) init();
 
-  Object.assign($vm, options, {
+  Object.assign($vm, {
+    // Reset to allow title or content be omitted
+    title: '',
+    content: ''
+  }, options, {
     show: 1,
     action(index) {
       $vm.show = 0;
-      options.action(index);
+      options.action && options.action(index);
     }
   });
 }
