f7ce2f9674d6f013a56ef5eea01009f499c3c0ac
Broltes <Broltes@Broltes-MacBook-Pro.local>
2017-06-27 11:01:34 +0800

    app-footer can set index

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="app-footer">
-    <div v-for="(item, i) in list"
-      :class="`app-footer-item${index === i ? ' app-footer-item-active' : ''}`"
-      @click="setIndex(i)">
+    <div v-for="item in items" :key="item.index"
+      :class="`app-footer-item${index === item.index ? ' app-footer-item-active' : ''}`"
+      @click="setIndex(item.index)">
 
       <span class="app-footer-icon">
-        <icon :id="index === i ? item.iconFilled || item.icon + '-fill' : item.icon"></icon>
+        <icon :id="index === item.index ? item.iconFilled || item.icon + '-fill' : item.icon"></icon>
         <span class="badge" v-if="item.badge">{{item.badge}}</span>
         <span class="badge-dot" v-if="item.badgeDot"></span>
       </span>
@@ -18,6 +18,15 @@
 <script>
 export default {
   props: ['list', 'index'],
+  computed: {
+    items() {
+      return this.list.map((item, i) => {
+        return Object.assign({
+          index: i
+        }, item);
+      })
+    }
+  },
   methods: {
     setIndex(index) {
       this.$emit('switch', index);
